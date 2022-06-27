e670063dfd3cbd669e2f8fda8bd221a888cb7006
Timur Khakkiev <khakkievtimur@yandex.ru>
2022-06-27 13:31:15 +0300

    add analytics page

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/analytics">Analytics</router-link>
     </nav>
-    <router-view />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
 
