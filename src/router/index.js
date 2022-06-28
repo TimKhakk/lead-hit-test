fa3aef243ecd8b788e8ba481edbb6796015c8d11
Timur Khakkiev <khakkievtimur@yandex.ru>
2022-06-28 09:13:36 +0300

    add private route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AnalyticsView from "../views/AnalyticsView.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -14,14 +15,12 @@ const routes = [
   {
     path: "/analytics",
     name: "analytics",
-    // beforeEnter: (to, from, next) => {
-    //   if (!localStorage.getItem("leadhit-site-id")) {
-    //     console.log("redirect")
-    //     next({ path: "/" });
-    //   } else {
-    //     next({ path: "/analytics" });
-    //   }
-    // },
+    beforeEnter: (to, from, next) => {
+      if (!store.state.isLoggedIn) {
+        alert("Вы не авторизованы");
+        next(false);
+      } else next();
+    },
     // route level code-splitting
     // this generates a separate chunk (analytics.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
